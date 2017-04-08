46ff61a649124ed0fdb3e741684f659fd467ebb5
z418577198 <418577198@qq.com>
2017-04-08 08:09:48 +0800

    change main.js

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
  * Created by zhangzhijun on 17/4/4.
  */
 'use strict'
-function drag(){
-    
+function drag(id){
+    var obj=document.getElementById(id);
+    obj.onmousedown=function(){
+
+    }
 }
